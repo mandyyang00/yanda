606836ff0e4c965bbce758f2a9f0d7dc7714e515
mandyyang00 <659890208@qq.com>
2017-10-20 13:48:55 +0800

    frist

--- src/component/Forgetpassword/Forgetpassword.js
+++ src/component/Forgetpassword/Forgetpassword.js
@@ -23,10 +23,35 @@ class Forgetpassword extends React.Component{
 				<Biaoti biaoti={this.state.biaoti}/>
 				<div>
 					<ul>
-						<li><span>手机号：</span></li>
-						<li><span>验证码：</span></li>
+						<li>
+							<Icon type="phone" style={{'color':'#1E90FF','fontSize':'14px'}}/>
+							<input type='input'  placeholder='请输入手机号' className='tel'/>
+						</li>
+						<li>
+							<Icon type="exception" style={{'color':'#1E90FF','fontSize':'14px'}}/>
+							<input type='text' style={{'width': '41%','line-height': '35px','border':'0px','outline':' 0','margin-left': '10px'}} placeholder='验证码' className='password'/>
+							<span type="primary" style={{'float':'right','lineHeight':'35px','color':'#1E90FF'}}>获取验证码</span>
+						</li>
+						
 					</ul>
+
+					<Button type="primary" style={{'marginTop':'30px'}}>提交验证码</Button>
+
+					<ul style={{'marginTop':'20px'}}>
+						<li>
+							<Icon type="edit" style={{'color':'#1E90FF','fontSize':'14px'}}/>
+							<input type='input'  placeholder='新密码（6位纯数字）' className='newpassword'/>
+						</li>
+						<li>
+							<Icon type="reload" style={{'color':'#1E90FF','fontSize':'14px'}}/>
+							<input type='text'  placeholder='确认新密码' className='resetpassword'/>
+							
+						</li>
+						
+					</ul>
+					<Button type="primary" style={{'marginTop':'30px'}}>提交新密码</Button>
 				</div>
+
 			</div>
 		)
 	}
